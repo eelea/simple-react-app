21024bcf45c2675cab0ad56b1dfa5f9414b28eb8
eelea <emmanuel_slr01@hotmail.com>
2020-08-02 17:39:31 -0500

    add Spinner component, it's used when getting info in Home

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import useAxiosGet from "../Hooks/HttpRequest";
+import Spinner from "../Components/Spinner";
 
 function Home() {
   const url = "http://localhost:3001/bikes";
@@ -9,6 +10,9 @@ function Home() {
 
   if (bikes.error) {
     content = <p>There was an error try again later.</p>;
+  }
+  if (bikes.loading) {
+    content = <Spinner />;
   }
   if (bikes.data) {
     content = bikes.data.map((bike, key) => <div>{bike.name}</div>);
