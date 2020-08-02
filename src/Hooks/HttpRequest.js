8baf7d9a112597a332f2494964d3b23cfd37e00d
eelea <emmanuel_slr01@hotmail.com>
2020-08-02 17:32:06 -0500

    add useAxiosGet hook for bikes request

---
+++ src/Hooks/HttpRequest.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function useAxiosGet(url) {
+  // const request, and setter we use useState hook
+  const [request, setRequest] = useState({
+    loading: false,
+    data: null,
+    error: false,
+  });
+
+  // useEffect to exec once
+  useEffect(() => {
+    setRequest({ loading: true, data: null, error: false });
+    axios
+      .get(url)
+      .then((resp) => {
+        setRequest({ loading: false, data: resp.data, error: false });
+      })
+      .catch((error) => {
+        setRequest({ loading: false, data: null, error: true });
+      });
+  }, [url]);
+
+  return request;
+}
+
+export default useAxiosGet;
