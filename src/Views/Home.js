01d8d93468af1cb80da2e00c4b6c46f94cc5923a
eelea <emmanuel_slr01@hotmail.com>
2020-08-02 17:33:21 -0500

    retrive bikes information in Home

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,7 +1,20 @@
 import React from "react";
+import useAxiosGet from "../Hooks/HttpRequest";
 
 function Home() {
-  return <div>Home</div>;
+  const url = "http://localhost:3001/bikes";
+
+  let bikes = useAxiosGet(url);
+  let content = null;
+
+  if (bikes.error) {
+    content = <p>There was an error try again later.</p>;
+  }
+  if (bikes.data) {
+    content = bikes.data.map((bike, key) => <div>{bike.name}</div>);
+  }
+
+  return <div>{content}</div>;
 }
 
 export default Home;
