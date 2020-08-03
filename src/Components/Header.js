fec29fa6f6d2999370ee39a242a56fc126fd6014
eelea <emmanuel_slr01@hotmail.com>
2020-08-02 21:29:20 -0500

    add link to home in Header component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Menu from "./Menu";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header className="border-b p-3 flex justify-between item-center">
       {/* Here we are using tailwind classes*/}
-      <span className="font-bold"> SimpleReactApp</span>
+      <Link to="/">
+        <span className="font-bold"> SimpleReactApp</span>
+      </Link>
       <Menu />
     </header>
   );
